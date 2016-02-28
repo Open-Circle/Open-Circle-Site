bba514944b54ff62cedaf8980ead499563a2cd9f
Jamie <gisellegk@gmail.com>
2016-02-28 02:59:40 -0500

    use ejs templating successfully on dashboard.html (with user navigation -- let me know if you have a better name). Need to separate CSS to be able to safely implement on a different page

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('view engine', 'ejs');
 
+// Serve Static Files
+app.use("/styles", express.static('styles'));
+app.use("/scripts", express.static('scripts'));
+app.use("/img", express.static('img'));
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// TODO: Make it send 404 HTTP code instead of that gross error
+app.get('/*', function (req, res) {
+  console.log(req.url);
+  var pageName = req.url.replace('/', '');
+  res.render(__dirname + "/views/" + pageName);
+});
 
 var server = http.listen(3000, function() {
 	var host = server.address().address;
